e868ab9518710a92520356619a4e026fed02f274
snegurova <snegurova@gmail.com>
2021-03-30 14:21:05 +0300

    add burger menu

--- uanimals/js/script.js
+++ uanimals/js/script.js
@@ -1,5 +1,5 @@
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
+const iconMenu = document.querySelector('.burger-menu__icon');
+const menuBody = document.querySelector('.burger-menu__body');
 if (iconMenu) {
     iconMenu.addEventListener("click", function (e) {
         document.body.classList.toggle('_lock');
