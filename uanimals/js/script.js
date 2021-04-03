278e447a0e3f12ea6829d7cf27b53e5d995589c1
snegurova <snegurova@gmail.com>
2021-04-03 22:50:39 +0300

    feat: mobile adaptive

--- uanimals/js/script.js
+++ uanimals/js/script.js
@@ -1,11 +1,69 @@
-const iconMenu = document.querySelector('.burger-menu__icon');
-const menuBody = document.querySelector('.burger-menu__body');
-if (iconMenu) {
+
+//Menu
+let unlock = true;
+let iconMenu = document.querySelector(".burger-menu__icon");
+if (iconMenu != null) {
+    let delay = 500;
+    let menuBody = document.querySelector(".burger-menu__body");
     iconMenu.addEventListener("click", function (e) {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
+        if (unlock) {
+            body_lock(delay);
+            iconMenu.classList.toggle("_active");
+            menuBody.classList.toggle("_active");
+        }
     });
+};
+function menu_close() {
+    let iconMenu = document.querySelector(".burger-menu__icon");
+    let menuBody = document.querySelector(".burger-menu__body");
+    iconMenu.classList.remove("_active");
+    menuBody.classList.remove("_active");
+}
+//=================
+//BodyLock
+function body_lock(delay) {
+    let body = document.querySelector("body");
+    if (body.classList.contains('_lock')) {
+        body_lock_remove(delay);
+    } else {
+        body_lock_add(delay);
+    }
+}
+function body_lock_remove(delay) {
+    let body = document.querySelector("body");
+    if (unlock) {
+        let lock_padding = document.querySelectorAll("._lp");
+        setTimeout(() => {
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = '0px';
+            }
+            body.style.paddingRight = '0px';
+            body.classList.remove("_lock");
+        }, delay);
+
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, delay);
+    }
+}
+function body_lock_add(delay) {
+    let body = document.querySelector("body");
+    if (unlock) {
+        let lock_padding = document.querySelectorAll("._lp");
+        for (let index = 0; index < lock_padding.length; index++) {
+            const el = lock_padding[index];
+            el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+        }
+        body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+        body.classList.add("_lock");
+
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, delay);
+    }
 }
 /**
  * Swiper 6.5.3
@@ -9619,9 +9677,10 @@ if (iconMenu) {
 //# sourceMappingURL=swiper-bundle.js.map
 
 
-const swiper = new Swiper('.swiper-container', {
+const lastNewsSlider = new Swiper('.last-news__slider', {
     // Optional parameters
     loop: true,
+    slidesPerView: 1,
 
     // If we need pagination
     pagination: {
@@ -9633,6 +9692,110 @@ const swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
     },
 });
+
+//---------
+
+const element = document.getElementById('tm');
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+const options = {
+    part: 0,
+    nextPart: 0.25,
+    size: 52,
+    lineWidth: 3,
+    decrease: false,
+    isPartToSet: true,
+}
+canvas.width = canvas.height = options.size;
+element.appendChild(canvas);
+
+const radius = Math.trunc(options.size / 2 - options.lineWidth);
+const center = options.size / 2;
+
+const drawCircle = (color, lineWidth, part = 1) => {
+    ctx.beginPath();
+    ctx.arc(center, center, radius,
+        -(Math.PI * 0.5), (Math.PI * 2) * part - Math.PI * 0.5, false);
+    ctx.strokeStyle = color;
+    // ctx.lineCap = 'round'; // butt, round or square
+    ctx.lineWidth = lineWidth
+    ctx.stroke();
+};
+
+const animateProgress = () => {
+    ctx.clearRect(0, 0, options.size, options.size);
+    ctx.save();
+    ctx.restore();
+    drawCircle('rgba(255, 255, 255, 0.3)', options.lineWidth);
+    drawCircle('#60CAE5', options.lineWidth, options.part);
+    if (options.decrease) {
+        if (options.part > options.nextPart) {
+            options.part *= 0.98;
+            window.requestAnimationFrame(animateProgress);
+        }
+    } else {
+        if (options.part < options.nextPart) {
+            options.part = options.part + 0.01;
+            window.requestAnimationFrame(animateProgress);
+        }
+    }
+}
+
+//------
+
+
+const projectsHomeSlider = new Swiper('.projects__slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 4.5,
+    centeredSlides: true,
+    initialSlide: 2,
+    pagination: {
+        el: '.home-projects-scrollbar',
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+
+            if (current === 1 && options.isPartToSet) {
+                options.part = 0;
+                options.isPartToSet = false;
+            }
+
+            if (current === 5) {
+                options.isPartToSet = true;
+            }
+
+            options.nextPart = current / total;
+
+            window.requestAnimationFrame(animateProgress);
+        }
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.home-projects-button-next',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1
+        },
+        // when window width is >= 480px
+        680: {
+            slidesPerView: 2
+        },
+        // when window width is >= 640px
+        992: {
+            slidesPerView: 3
+        },
+        1366: {
+            slidesPerView: 4
+        },
+        1600: {
+            slidesPerView: 4.5
+        },
+    },
+});
+
 
 function testWebP(callback) {
 	var webP = new Image();
