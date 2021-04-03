003b9e2d39c94cb463675eba4ecb134390c146dd
snegurova <snegurova@gmail.com>
2021-04-03 14:17:35 +0300

    fix: body lock, images path

--- uanimals/js/script.js
+++ uanimals/js/script.js
@@ -1,11 +1,69 @@
-const iconMenu = document.querySelector('.burger-menu__icon');
-const menuBody = document.querySelector('.burger-menu__body');
-if (iconMenu) {
+
+//Menu
+let unlock = true;
+let iconMenu = document.querySelector(".burger-menu__icon");
+if (iconMenu != null) {
+    let delay = 500;
+    let menuBody = document.querySelector(".burger-menu__body");
     iconMenu.addEventListener("click", function (e) {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
+        if (unlock) {
+            body_lock(delay);
+            iconMenu.classList.toggle("_active");
+            menuBody.classList.toggle("_active");
+        }
     });
+};
+function menu_close() {
+    let iconMenu = document.querySelector(".burger-menu__icon");
+    let menuBody = document.querySelector(".burger-menu__body");
+    iconMenu.classList.remove("_active");
+    menuBody.classList.remove("_active");
+}
+//=================
+//BodyLock
+function body_lock(delay) {
+    let body = document.querySelector("body");
+    if (body.classList.contains('_lock')) {
+        body_lock_remove(delay);
+    } else {
+        body_lock_add(delay);
+    }
+}
+function body_lock_remove(delay) {
+    let body = document.querySelector("body");
+    if (unlock) {
+        let lock_padding = document.querySelectorAll("._lp");
+        setTimeout(() => {
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = '0px';
+            }
+            body.style.paddingRight = '0px';
+            body.classList.remove("_lock");
+        }, delay);
+
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, delay);
+    }
+}
+function body_lock_add(delay) {
+    let body = document.querySelector("body");
+    if (unlock) {
+        let lock_padding = document.querySelectorAll("._lp");
+        for (let index = 0; index < lock_padding.length; index++) {
+            const el = lock_padding[index];
+            el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+        }
+        body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+        body.classList.add("_lock");
+
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, delay);
+    }
 }
 /**
  * Swiper 6.5.3
