1a6d96c6a50a4d46ad24428fe095b3f73a4427c5
snegurova <snegurova@gmail.com>
2021-04-03 18:56:20 +0300

    feat: add slider circle progress

--- #src/js/sliders.js
+++ #src/js/sliders.js
@@ -1,6 +1,6 @@
 @@include('./libs/swiper-bundle.js', {})
 
-const swiper = new Swiper('.swiper-container', {
+const lastNewsSlider = new Swiper('.last-news__slider', {
     // Optional parameters
     loop: true,
 
@@ -13,4 +13,98 @@ const swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
     },
+});
+
+//---------
+
+const element = document.getElementById('tm');
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+const options = {
+    part: 0,
+    nextPart: 0.25,
+    size: 52,
+    lineWidth: 3,
+    decrease: false,
+    isPartToSet: true,
+}
+canvas.width = canvas.height = options.size;
+element.appendChild(canvas);
+
+const radius = Math.trunc(options.size / 2 - options.lineWidth);
+const center = options.size / 2;
+
+const drawCircle = (color, lineWidth, part = 1) => {
+    ctx.beginPath();
+    ctx.arc(center, center, radius,
+        -(Math.PI * 0.5), (Math.PI * 2) * part - Math.PI * 0.5, false);
+    ctx.strokeStyle = color;
+    // ctx.lineCap = 'round'; // butt, round or square
+    ctx.lineWidth = lineWidth
+    ctx.stroke();
+};
+
+const animateProgress = () => {
+    ctx.clearRect(0, 0, options.size, options.size);
+    ctx.save();
+    ctx.restore();
+    drawCircle('rgba(255, 255, 255, 0.3)', options.lineWidth);
+    drawCircle('#60CAE5', options.lineWidth, options.part);
+    if (options.decrease) {
+        if (options.part > options.nextPart) {
+            options.part *= 0.98;
+            window.requestAnimationFrame(animateProgress);
+        }
+    } else {
+        if (options.part < options.nextPart) {
+            options.part = options.part + 0.01;
+            window.requestAnimationFrame(animateProgress);
+        }
+    }
+}
+
+//------
+
+
+const projectsHomeSlider = new Swiper('.projects__slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 4.5,
+    centeredSlides: true,
+    initialSlide: 2,
+    pagination: {
+        el: '.home-projects-scrollbar',
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+
+            if (current === 1 && options.isPartToSet) {
+                options.part = 0;
+                options.isPartToSet = false;
+            }
+
+            if (current === 5) {
+                options.isPartToSet = true;
+            }
+
+            options.nextPart = current / total;
+
+            window.requestAnimationFrame(animateProgress);
+            console.log (current, total);
+        }
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.home-projects-button-next',
+    },
+});
+projectsHomeSlider.on('slideChange', function () {
+    const progressEl = document.querySelector('.progress');
+    const progress = projectsHomeSlider.progress * 180;
+    progressEl.style.transform = 'rotate('+ progress +'deg)';
+});
+projectsHomeSlider.on('afterInit', function () {
+    const progressEl = document.querySelector('.progress');
+    const progress = projectsHomeSlider.progress * 360;
+    progressEl.style.transform = 'rotate('+ progress +'deg)';
 });
