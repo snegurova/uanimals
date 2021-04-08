2a1b2b3d9647368a04aae0ae30761c72e772260e
snegurova <snegurova@gmail.com>
2021-04-08 23:04:54 +0300

    fix: home page adaptive

--- uanimals/js/script.js
+++ uanimals/js/script.js
@@ -10209,10 +10209,12 @@ const lastNewsSlider = new Swiper('.last-news__slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
+    effect: 'fade',
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
     },
 
     // Navigation arrows
